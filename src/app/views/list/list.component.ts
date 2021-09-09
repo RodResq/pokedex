4e8ad62360a73a064013ea3bc5ac8e80a1051828
RodResq <rodrigoresque@yahoo.com.br>
2021-09-09 20:37:39 -0300

    updt

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  selectedPkm: null;
+
+  pokemonList = [
+    {name: 'Bulbasaur', number: 1},
+    {name: 'Chamander', number: 4},
+    {name: 'Squirtle', number: 7},
+    {name: 'Pikachu', number: 25},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  get pkmSprite() {
+    // @ts-ignore
+    const number = ('000' + this.selectedPkm.number).slice(-3)
+    return `//serebii.net/sunmoon/pokemon/${number}.png`;
+  }
+
+  // tslint:disable-next-line:typedef
+  selectPkm(pkm: any) {
+    this.selectedPkm = pkm;
+  }
 }
